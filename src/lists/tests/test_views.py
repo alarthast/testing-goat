892280cb0cad861720c2fc79c5768a537c839014
Alice Wong <alice.wong@thedatalab.org>
2024-12-12 22:57:50 +0000

    Split out unit tests into two files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -78,32 +78,3 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
         del other_list
-
-
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
