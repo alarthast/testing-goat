2d778a2a4b699db2d422e9e97bdbc9013c104ad6
Alice Wong <alice.wong@thedatalab.org>
2024-12-12 17:49:59 +0000

    Add favicon since the browser complains otherwise

--- src/superlists/urls.py
+++ src/superlists/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from lists import views as list_views
 
@@ -6,4 +8,8 @@ from lists import views as list_views
 urlpatterns = [
     path("", list_views.home_page, name="home"),
     path("lists/", include("lists.urls")),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon/favicon.ico")),
+    ),
 ]
