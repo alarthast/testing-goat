d36118d3578ce5c1b5f073ddbfe6c26bae8509f3
Alice Wong <alice.wong@thedatalab.org>
2024-12-17 13:58:30 +0000

    Juggle order to match book

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -38,6 +38,31 @@ class NewListTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        mylist = List.objects.create()
+        response = self.client.get(f"/lists/{mylist.id}/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=correct_list)
+        Item.objects.create(text="itemey 2", list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text="other list item", list=other_list)
+
+        response = self.client.get(f"/lists/{correct_list.id}/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+        self.assertNotContains(response, "other list item")
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context["list"], correct_list)
+        del other_list
+
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -63,29 +88,4 @@ class ListViewTest(TestCase):
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-        del other_list
-
-    def test_uses_list_template(self):
-        mylist = List.objects.create()
-        response = self.client.get(f"/lists/{mylist.id}/")
-        self.assertTemplateUsed(response, "list.html")
-
-    def test_displays_only_items_for_that_list(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text="itemey 1", list=correct_list)
-        Item.objects.create(text="itemey 2", list=correct_list)
-        other_list = List.objects.create()
-        Item.objects.create(text="other list item", list=other_list)
-
-        response = self.client.get(f"/lists/{correct_list.id}/")
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
-        self.assertNotContains(response, "other list item")
-
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f"/lists/{correct_list.id}/")
-        self.assertEqual(response.context["list"], correct_list)
         del other_list
