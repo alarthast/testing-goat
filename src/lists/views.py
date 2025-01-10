7be4b8ebf47fb914b8ae01f0a4f69daede39a6a8
Alice Wong <alice.wong@thedatalab.org>
2025-01-10 13:18:17 +0000

    Implement custom save method for the form

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 from lists.forms import ItemForm
-from lists.models import Item, List
+from lists.models import List
 
 
 # Create your views here.
@@ -13,7 +13,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         nulist = List.objects.create()
-        Item.objects.create(text=request.POST["text"], list=nulist)
+        form.save(for_list=nulist)
         return redirect(nulist)
     else:
         return render(request, "home.html", {"form": form})
@@ -24,7 +24,7 @@ def view_list(request, list_id):
     if request.method == "POST":
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST["text"], list=our_list)
+            form.save(for_list=our_list)
             return redirect(our_list)
     else:
         form = ItemForm()
