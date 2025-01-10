d9ff7c88d449dfd5d82c9c0c9cc132c5fbebccf3
Alice Wong <alice.wong@thedatalab.org>
2025-01-10 12:33:00 +0000

    Rename all item input ids and names. Still broken

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -6,7 +6,6 @@ from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 class ItemFormTest(TestCase):
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
-        # self.fail(form.as_p())
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control form-control-lg"', form.as_p())
 
