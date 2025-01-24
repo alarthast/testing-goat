63712cb7cb4ce7064e76946bb515b7185cb80a45
Alice Wong <alice.wong@thedatalab.org>
2025-01-24 13:10:29 +0000

    Fix list item ordering

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -45,6 +45,20 @@ class ItemModelTest(TestCase):
         item = Item(list=list2, text="bla")
         item.full_clean()  # should not raise
 
+    def test_string_representation(self):
+        item = Item(text="some text")
+        self.assertEqual(str(item), "some text")
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text="i1")
+        item2 = Item.objects.create(list=list1, text="item 2")
+        item3 = Item.objects.create(list=list1, text="3")
+        self.assertEqual(
+            list(list1.item_set.all()),
+            [item1, item2, item3],
+        )
+
 
 class ListModelTest(TestCase):
     def test_get_absolute_url(self):
