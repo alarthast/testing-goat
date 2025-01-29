2b2100f4c8a0a368e0a85c703be8e584e09be9a2
Alice Wong <alice.wong@thedatalab.org>
2025-01-29 14:33:46 +0000

    Add jasmine unit tests for error element (failing)

--- src/lists/static/tests/Spec.js
+++ src/lists/static/tests/Spec.js
@@ -1,7 +1,41 @@
 describe("Superlists tests", () => {
+  let testDiv;
 
-  it("smoke test", () => {
-    expect(1 + 1).toEqual(2);
+  beforeEach(() => {
+    testDiv = document.createElement("div");
+    testDiv.innerHTML = `
+      <form>
+        <input
+          id="id_text"
+          name="text"
+          class="form-control form-control-lg is-invalid"
+          placeholder="Enter a to-do item"
+          value="Value as submitted"
+          aria-describedby="id_text_feedback"
+          required
+        />
+        <div id="id_text_feedback" class="invalid-feedback">An error message</div>
+      </form>
+    `;
+    document.body.appendChild(testDiv);
+  });
+
+  afterEach(() => {
+    testDiv.remove();
+  });
+
+  it("sense-check our html fixture", () => {
+    const errorMsg = document.querySelector(".invalid-feedback");
+    expect(errorMsg.checkVisibility()).toBe(true);
+  });
+
+  it("error message should be hidden on input", () => {
+    const textInput = document.querySelector("#id_text");
+    const errorMsg = document.querySelector(".invalid-feedback");
+
+    textInput.dispatchEvent(new InputEvent("input"));
+
+    expect(errorMsg.checkVisibility()).toBe(false);
   });
 
 });
