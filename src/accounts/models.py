00e07c31ec6f8d1ebe371caa9d7c35e68816669b
Alice Wong <alice.wong@thedatalab.org>
2025-02-17 16:24:09 +0000

    Add `ListUser` model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,27 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+)
 from django.db import models
+
+from superlists import settings
+
+
+STAFF_EMAIL = settings.DEFAULT_FROM_EMAIL
 
 
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(max_length=255)
+
+
+class ListUser(AbstractBaseUser):
+    email = models.EmailField(primary_key=True)
+    USERNAME_FIELD = "email"
+
+    @property
+    def is_staff(self):
+        return self.email == STAFF_EMAIL
+
+    @property
+    def is_active(self):
+        return True
