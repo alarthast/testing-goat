c0f1077f148fe89d7f264c73da489cfad77c82e0
Alice Wong <alice.wong@thedatalab.org>
2025-02-21 14:30:05 +0000

    Add `User` model (Green test)

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    is_anonymous = False
+    is_authenticated = True
